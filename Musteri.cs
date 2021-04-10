55730f06477a681f38f99e2fa1a28fa532a28502
Yucel <yucel.olmez94@hotmail.com>
2021-04-10 21:25:16 +0300

    Add project files.

---
+++ Musteri.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class Musteri
+    {
+
+        public string tc { get; set; }
+        public string adi { get; set; }
+        public string soyadi { get; set; }
+        public string dogumTarihi { get; set; }
+        public int yas { get; set; }
+        public string dogumYeri { get; set; }
+
+
+
+    }
+}
